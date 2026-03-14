9664ccf24dea16bd855875a86271c1c24dc413b6
Akash Gupta <28701869+aakash1147@users.noreply.github.com>
2026-03-15 02:04:29 +0530

    code

--- food-app-angular/src/app/auth/login/login.component.ts
+++ food-app-angular/src/app/auth/login/login.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MaterialModule } from '../../material.module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, MaterialModule],
+  imports: [CommonModule, ReactiveFormsModule, MaterialModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
--- food-app-angular/src/app/auth/register/register.component.ts
+++ food-app-angular/src/app/auth/register/register.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MaterialModule } from '../../material.module';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, MaterialModule],
+  imports: [CommonModule, ReactiveFormsModule, MaterialModule,RouterModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
